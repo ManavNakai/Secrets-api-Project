a8defef4baa455bfadbdf2c7c1d82b6e191b6c12
Manav Nakai <118651765+ManavNakai@users.noreply.github.com>
2024-06-27 16:43:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+    try {
+    const response = await axios.get("https://secrets-api.appbrewery.com/random");
+    const username= response.data.username;
+    const secret= response.data.secret;
+    res.render("index.ejs", { secret: secret, user: username});
+    } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
